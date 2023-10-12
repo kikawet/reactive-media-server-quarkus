d0879a5b9d3a0a5309774e7a83f602f9e42b67d0
kikawet <lopezortegafrancisco1@gmail.com>
2023-10-12 21:34:37 +0200

    basic authorization implemented

--- src/main/java/rms/resources/UserResource.java
+++ src/main/java/rms/resources/UserResource.java
@@ -2,20 +2,42 @@ package rms.resources;
 
 import java.util.List;
 
+import org.apache.http.HttpStatus;
+
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
 import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import rms.model.Suggestion;
 import rms.model.User;
 import rms.model.UserView;
 
 @Path("user")
+@Authenticated
 public class UserResource {
+        @Inject
+        SecurityIdentity identity;
+
         @GET
         @Path("{userId}/history")
         public Uni<List<UserView>> getHistoryByUser(@PathParam("userId") String userId) {
+                if (!identity.hasRole("admin") && !identity.getPrincipal().getName().equals(userId))
+                        throw new ForbiddenException(Response
+                                        .status(HttpStatus.SC_FORBIDDEN)
+                                        .type(MediaType.TEXT_PLAIN)
+                                        .entity(
+                                                        "The path userId: '" + userId
+                                                                        + "' does not match the authentication userId: '"
+                                                                        + identity.getPrincipal().getName() + "'")
+                                        .build());
+
                 return User.findById(userId)
                                 .onItem().ifNull().failWith(new NotFoundException())
                                 .flatMap(x -> UserView
