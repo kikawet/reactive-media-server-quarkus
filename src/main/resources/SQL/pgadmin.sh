5819125d91655d3b7ce955916f5ad2ca4cb20dca
kikawet <lopezortegafrancisco1@gmail.com>
2023-10-12 12:48:52 +0200

    persist pgadmin + name container

--- src/main/resources/SQL/pgadmin.sh
+++ src/main/resources/SQL/pgadmin.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 set -xe
 
+CONTAINERNAME=${CONTAINERNAME:-pgadmin}
+
 docker pull dpage/pgadmin4
-docker run -p 5050:80 \
+docker container inspect $CONTAINERNAME &>/dev/null && docker rm $CONTAINERNAME
+docker run \
+    --name $CONTAINERNAME \
+    --volume ./../../../../local/dev/pgadmin:/var/lib/pgadmin \
+    -p 5050:80 \
     -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' \
     -e 'PGADMIN_DEFAULT_PASSWORD=SuperSecret' \
     -e 'PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION=True' \
     -e 'PGADMIN_CONFIG_LOGIN_BANNER="Authorised users only!"' \
     -e 'PGADMIN_CONFIG_CONSOLE_LOG_LEVEL=10' \
     -d dpage/pgadmin4 \
-    --name pgadmin
